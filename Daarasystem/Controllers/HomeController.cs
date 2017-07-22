9996989e15e2d893727a4ef344ec362e4751f28a
falou10 <falou10@yahoo.fr>
2017-07-23 01:19:39 +0200

    dff
    fffff

---
+++ Daarasystem/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Daarasystem.Toubasoft.Service;
+
+namespace Daarasystem.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+            ServiceSOSSoapClient servie = new ServiceSOSSoapClient();
+            var op =  servie.AddEleve("", "", "");
+            
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ Toubasoft.WebService/ServiceSOS.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Toubasoft.WebService
+{
+    /// <summary>
+    /// Description résumée de ServiceSOS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServiceSOS : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string AddEleve(string nom, string prenom, string addr)
+        {
+            //Implementation des requettes
+            return "Hello World";
+        }
+    }
+}
